5fadc4cff572a40435bb23f0fbaed560ab29afb6
ark202625 <aryankadu255@gmail.com>
2026-07-31 02:57:43 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ app.listen(PORT, () => {
 });
 
 // Target Server Config
-const HOST = process.env.MC_HOST || 'cuis.aternos.host';
+const HOST = process.env.MC_HOST || 'piximc.aternos.me';
 const PORT_MC = parseInt(process.env.MC_PORT) || 43180;
 const BOT_NAME = process.env.MC_USERNAME || 'pixi'; 
 
